68b0ed9f867c3dff4b23cc75af746e7278c7ae25
asif p ashraf <asifpashraf@gmail.com>
2024-08-28 13:21:44 +0530

    cotact updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,11 @@ function Header() {
   return (
     <div>
       <h1>MKS SOLUTIONS</h1>
-      <h4>slow and safe</h4>
+      <h4>fast and safe</h4>
+      <h4>pack your home safely shift your home in speed</h4>
+      <p>contact</p>
+      <p>7510894410</p>
+      <p>complete your travel</p>
     </div>
   )
 }
